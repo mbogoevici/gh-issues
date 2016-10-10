72d211a6c44737a745d1abb028e2d7994a7f31f5
Marius Bogoevici <mbogoevici@pivotal.io>
2016-10-10 19:52:16 -0400

    Added example for GH-683

---
+++ gh-683/src/main/java/ca/testcomponent/DemoApplication.java
@@ -0,0 +1,13 @@
+package ca.testcomponent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
